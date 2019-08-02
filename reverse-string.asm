0da77399e32b4133708614630733ccfac8098005
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-08-02 14:50:33 -0700

    first commit

---
+++ reverse-string.asm
@@ -0,0 +1,10 @@
+TITLE DemoString(demo6.asm)
+
+;Author:		
+;CS271			in-class demo
+;Description:	This program asks the user to enter a string,
+;               and then displays the string in all caps.
+;               Finally, the string is displayed backwards.
+
+INCLUDE Irvine32.inc
+MAXSIZE = 100
